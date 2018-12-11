38b9d67a68285e69ea4b26ddc50e5b05a1af9fec
jerh56 <jerh56@gmail.com>
2018-12-10 23:05:26 -0700

    Update uploadimages.js

--- www/public/js/uploadimages.js
+++ www/public/js/uploadimages.js
@@ -38,7 +38,7 @@ $(document).ready(function(){
                     }
                 }
             }
-            $.getJSON('/sign-s3-upload', params).done(function(res) {
+            $.getJSON('https://apptdfront.herokuapp.com/sign-s3-upload', params).done(function(res) {
                 if (!res.policy) {
                     return cb('No se pudo recibir una URL para cargar la imagen');
                 }
